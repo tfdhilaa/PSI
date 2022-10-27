3a6f100c5bb9b0e1468547b0faeda64f2fbb1e64
tfdhilaa <thanafadhila08@gmail.com>
2022-10-27 23:40:41 +0700

    first commit

--- app/src/main/java/com/example/psi_recyclerview/CustomAdapter.kt
+++ app/src/main/java/com/example/psi_recyclerview/CustomAdapter.kt
@@ -3,8 +3,6 @@ package com.example.psi_recyclerview
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.psi_recyclerview.ItemsViewModel
-import com.example.psi_recyclerview.Nama
 
 class CustomAdapter(private val data: ArrayList<Nama>): RecyclerView.Adapter<ItemsViewModel>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemsViewModel {
@@ -21,33 +19,3 @@ class CustomAdapter(private val data: ArrayList<Nama>): RecyclerView.Adapter<Ite
     }
 }
 
-//import android.view.LayoutInflater
-//import android.view.View
-//import android.view.ViewGroup
-//import android.widget.ImageView
-//import android.widget.TextView
-//import androidx.recyclerview.widget.RecyclerView
-//import com.example.psi_recyclerview.ItemsViewModel
-//import com.example.psi_recyclerview.R
-//
-//class CustomAdapter(private val mList: List<ItemsViewModel>) : RecyclerView.Adapter<CustomAdapter.ListViewHolder>() {
-//    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-//        var Nama : TextView = itemView.findViewById(R.id.nama)
-//        var Nim : TextView = itemView.findViewById(R.id.nim)
-//        var Kelas : TextView = itemView.findViewById(R.id.kelas)
-//    }
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-//        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.card_view_design, parent, false)
-//        return CustomAdapter.ListViewHolder(view)
-//    }
-//
-//    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-//        val x = mList[position]
-//        holder.Nama.text = x.Nama
-//        holder.Nim.text = x.NIM
-//        holder.Kelas.text = x.Kelas
-//    }
-//
-//    override fun getItemCount() = mList.size
-//}
